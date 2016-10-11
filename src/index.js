34549bd945779ba997c9536f1ba2c80382a52ff7
Anurag Aryan <anurag.aryan2011@gmail.com>
2016-10-11 16:56:28 +0530

    added states

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
-const App = () => {
-  return <div>Lets start</div>;
+const API_KEY = 'AIzaSyCebe45gBhsjqE7hLjl73VI4BdJ8dOj4rs';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {videos: [] };
+
+    YTSearch({key: API_KEY, term: 'Led Zeppelin'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
