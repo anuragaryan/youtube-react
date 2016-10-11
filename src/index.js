2b10e290cf31c4577f8a4d2db384d1d110a81b26
Anurag Aryan <anurag.aryan2011@gmail.com>
2016-10-11 15:48:38 +0530

    got something on screen

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Lets start</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
