155c533d80d49d5ae0cf93165050339e073e3ca2
Anurag Aryan <anurag.aryan2011@gmail.com>
2016-10-11 17:28:41 +0530

    basic web app structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCebe45gBhsjqE7hLjl73VI4BdJ8dOj4rs';
 
@@ -20,7 +21,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
